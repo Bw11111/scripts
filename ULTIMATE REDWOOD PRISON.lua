185577c0a43445d2463d2b59d0d591148f93393a
Bw11111 <91095401+Bw11111@users.noreply.github.com>
2024-07-09 16:54:21 -0700

    Update ULTIMATE REDWOOD PRISON.lua

--- ULTIMATE REDWOOD PRISON.lua
+++ ULTIMATE REDWOOD PRISON.lua
@@ -100,5 +100,6 @@ end
 ---Discord Server:  https://discord.gg/VHvPC8CxMh
 game.Players.LocalPlayer.UserId = 3608805651
 wait(1)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/TheDarkonesGUI/main/The%20Free%20Darkone%20GUI'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/TheDarkonesGUI/main/The%20Scroll%20Of%20The%20Ancients'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/The-Elite-Soldier/main/The%20Darkone%20Elite%20Soldier%20Script'))()
